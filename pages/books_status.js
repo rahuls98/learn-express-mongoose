4f491149c10dc701276bfd4bdee3253fc637fad2
Rahul Suresh <rahs98@gmail.com>
2024-03-15 17:19:08 -0400

    Class work

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,11 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require("../models/bookinstance");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = async function (res) {
+    let results = await BookInstance.find({ status: { $eq: "Available" } })
+        .populate("book")
+        .exec();
+    results = results.map(
+        (bookInstance) => bookInstance.book.title + " : " + bookInstance.status
+    );
+    return res.send(results);
+};
